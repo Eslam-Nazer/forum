aa03e7fd6e938b6159dad9e56b6db251bc071144
Eslam-Nazer <eslam.nazer43@gmail.com>
2025-10-09 03:46:06 +0300

    Updates route definitions and removes unused destroy methods
    Adjusts route definitions to reflect updated references and paths, ensuring consistency with underlying controller changes.
    
    Removes obsolete destroy methods from FavoriteController to streamline the codebase. Updates documentation comments across multiple files to match updated controller line numbers.
    
    Enhances maintainability by eliminating redundant code and aligning routes with the latest backend API structure.

--- resources/js/routes/favorite/index.ts
+++ resources/js/routes/favorite/index.ts
@@ -1,8 +1,8 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../wayfinder'
 /**
 * @see \Modules\Forum\Http\Controllers\FavoriteController::store
-* @see modules/Forum/Http/Controllers/FavoriteController.php:27
-* @route '/favorites/{type}/{id}'
+* @see modules/Forum/Http/Controllers/FavoriteController.php:41
+* @route '/{type}/{id}/favorites'
 */
 export const store = (args: { type: string | number, id: string | number } | [type: string | number, id: string | number ], options?: RouteQueryOptions): RouteDefinition<'post'> => ({
     url: store.url(args, options),
@@ -11,13 +11,13 @@ export const store = (args: { type: string | number, id: string | number } | [ty
 
 store.definition = {
     methods: ["post"],
-    url: '/favorites/{type}/{id}',
+    url: '/{type}/{id}/favorites',
 } satisfies RouteDefinition<["post"]>
 
 /**
 * @see \Modules\Forum\Http\Controllers\FavoriteController::store
-* @see modules/Forum/Http/Controllers/FavoriteController.php:27
-* @route '/favorites/{type}/{id}'
+* @see modules/Forum/Http/Controllers/FavoriteController.php:41
+* @route '/{type}/{id}/favorites'
 */
 store.url = (args: { type: string | number, id: string | number } | [type: string | number, id: string | number ], options?: RouteQueryOptions) => {
     if (Array.isArray(args)) {
@@ -42,8 +42,8 @@ store.url = (args: { type: string | number, id: string | number } | [type: strin
 
 /**
 * @see \Modules\Forum\Http\Controllers\FavoriteController::store
-* @see modules/Forum/Http/Controllers/FavoriteController.php:27
-* @route '/favorites/{type}/{id}'
+* @see modules/Forum/Http/Controllers/FavoriteController.php:41
+* @route '/{type}/{id}/favorites'
 */
 store.post = (args: { type: string | number, id: string | number } | [type: string | number, id: string | number ], options?: RouteQueryOptions): RouteDefinition<'post'> => ({
     url: store.url(args, options),
@@ -52,8 +52,8 @@ store.post = (args: { type: string | number, id: string | number } | [type: stri
 
 /**
 * @see \Modules\Forum\Http\Controllers\FavoriteController::store
-* @see modules/Forum/Http/Controllers/FavoriteController.php:27
-* @route '/favorites/{type}/{id}'
+* @see modules/Forum/Http/Controllers/FavoriteController.php:41
+* @route '/{type}/{id}/favorites'
 */
 const storeForm = (args: { type: string | number, id: string | number } | [type: string | number, id: string | number ], options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
     action: store.url(args, options),
@@ -62,8 +62,8 @@ const storeForm = (args: { type: string | number, id: string | number } | [type:
 
 /**
 * @see \Modules\Forum\Http\Controllers\FavoriteController::store
-* @see modules/Forum/Http/Controllers/FavoriteController.php:27
-* @route '/favorites/{type}/{id}'
+* @see modules/Forum/Http/Controllers/FavoriteController.php:41
+* @route '/{type}/{id}/favorites'
 */
 storeForm.post = (args: { type: string | number, id: string | number } | [type: string | number, id: string | number ], options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
     action: store.url(args, options),
@@ -72,92 +72,8 @@ storeForm.post = (args: { type: string | number, id: string | number } | [type: 
 
 store.form = storeForm
 
-/**
-* @see \Modules\Forum\Http\Controllers\FavoriteController::destroy
-* @see modules/Forum/Http/Controllers/FavoriteController.php:40
-* @route '/favorites/{type}/{id}'
-*/
-export const destroy = (args: { type: string | number, id: string | number } | [type: string | number, id: string | number ], options?: RouteQueryOptions): RouteDefinition<'delete'> => ({
-    url: destroy.url(args, options),
-    method: 'delete',
-})
-
-destroy.definition = {
-    methods: ["delete"],
-    url: '/favorites/{type}/{id}',
-} satisfies RouteDefinition<["delete"]>
-
-/**
-* @see \Modules\Forum\Http\Controllers\FavoriteController::destroy
-* @see modules/Forum/Http/Controllers/FavoriteController.php:40
-* @route '/favorites/{type}/{id}'
-*/
-destroy.url = (args: { type: string | number, id: string | number } | [type: string | number, id: string | number ], options?: RouteQueryOptions) => {
-    if (Array.isArray(args)) {
-        args = {
-            type: args[0],
-            id: args[1],
-        }
-    }
-
-    args = applyUrlDefaults(args)
-
-    const parsedArgs = {
-        type: args.type,
-        id: args.id,
-    }
-
-    return destroy.definition.url
-            .replace('{type}', parsedArgs.type.toString())
-            .replace('{id}', parsedArgs.id.toString())
-            .replace(/\/+$/, '') + queryParams(options)
-}
-
-/**
-* @see \Modules\Forum\Http\Controllers\FavoriteController::destroy
-* @see modules/Forum/Http/Controllers/FavoriteController.php:40
-* @route '/favorites/{type}/{id}'
-*/
-destroy.delete = (args: { type: string | number, id: string | number } | [type: string | number, id: string | number ], options?: RouteQueryOptions): RouteDefinition<'delete'> => ({
-    url: destroy.url(args, options),
-    method: 'delete',
-})
-
-/**
-* @see \Modules\Forum\Http\Controllers\FavoriteController::destroy
-* @see modules/Forum/Http/Controllers/FavoriteController.php:40
-* @route '/favorites/{type}/{id}'
-*/
-const destroyForm = (args: { type: string | number, id: string | number } | [type: string | number, id: string | number ], options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
-    action: destroy.url(args, {
-        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
-            _method: 'DELETE',
-            ...(options?.query ?? options?.mergeQuery ?? {}),
-        }
-    }),
-    method: 'post',
-})
-
-/**
-* @see \Modules\Forum\Http\Controllers\FavoriteController::destroy
-* @see modules/Forum/Http/Controllers/FavoriteController.php:40
-* @route '/favorites/{type}/{id}'
-*/
-destroyForm.delete = (args: { type: string | number, id: string | number } | [type: string | number, id: string | number ], options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
-    action: destroy.url(args, {
-        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
-            _method: 'DELETE',
-            ...(options?.query ?? options?.mergeQuery ?? {}),
-        }
-    }),
-    method: 'post',
-})
-
-destroy.form = destroyForm
-
 const favorite = {
     store: Object.assign(store, store),
-    destroy: Object.assign(destroy, destroy),
 }
 
 export default favorite
