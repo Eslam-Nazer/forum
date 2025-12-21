cfa31bca67c5259faf377967117fb7163d5eabee
Eslam-Nazer <eslam.nazer43@gmail.com>
2025-12-21 09:47:22 +0200

    Rename AllThreadsFilteredDto to ThreadsFilteredDto to standard naming     - This change improves consistency in DTO naming conventions.

--- modules/Forum/Application/UseCases/Thread/ThreadsUseCase.php
+++ modules/Forum/Application/UseCases/Thread/ThreadsUseCase.php
@@ -4,7 +4,7 @@ namespace Modules\Forum\Application\UseCases\Thread;
 
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
-use Modules\Forum\Application\DTOs\Thread\AllThreadsFilteredDto;
+use Modules\Forum\Application\DTOs\Thread\ThreadsFilteredDto;
 use Modules\Forum\Domain\Models\Thread;
 use Modules\Forum\Domain\Repositories\Thread\Filters\FilterThreadsRepositoryInterface;
 use Modules\Forum\Domain\Repositories\Thread\ThreadsRepositoryInterface;
@@ -16,17 +16,12 @@ class ThreadsUseCase
         private readonly FilterThreadsRepositoryInterface $filterThreadsRepository,
     ) {}
 
-    public function execute(AllThreadsFilteredDto $dto): Collection|LengthAwarePaginator
+    public function execute(ThreadsFilteredDto $dto): Collection|LengthAwarePaginator
     {
         $threads = $this->threadsRepository->handle($dto->channel);
 
         $threads = $this->filterThreadsRepository->apply($threads, request());
 
-        $threads->each(function (Thread $thread) {
-            return $thread->append('visits_count');
-        });
-
-
-        return $threads->paginate(5);
+        return $threads->paginate(5)->through(fn ($thread)=> $thread->append('visits_count'));
     }
 }
