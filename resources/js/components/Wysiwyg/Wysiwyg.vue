1ba4dc74bf00250e783d63fb3b0feed31f4915c7
Eslam-Nazer <eslam.nazer43@gmail.com>
2026-01-13 11:17:21 +0200

    Change tiptap editor action buttons to icons (lucide)

--- resources/js/components/Wysiwyg/Wysiwyg.vue
+++ resources/js/components/Wysiwyg/Wysiwyg.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts">
 import { useEditor, EditorContent } from '@tiptap/vue-3';
 import StarterKit from '@tiptap/starter-kit';
+import {
+    Bold,
+    Italic,
+    Underline,
+    Heading1,
+    Heading2,
+    List,
+    ListOrdered,
+    Code,
+    Quote,
+    SquareMinus,
+    Undo,
+    Redo
+} from 'lucide-vue-next';
+import { Button } from '@/components/ui/button';
 
 const editor = useEditor({
     content: '<p>I\'m running Tiptap with Vue.js. 🎉</p>',
@@ -19,99 +34,111 @@ const editor = useEditor({
     <div class="container mx-auto my-8">
         <section v-if="editor"
                  class="felx items-center flex-wrap gap-x-4 border-t border-r border-l border-gray-400 p-4">
-            <button
+            <Button
+                variant="outline"
                 @click="editor.chain().focus().toggleBold().run()"
                 :disabled="!editor.can().chain().focus().toggleBold().run()"
                 :class="{ 'bg-gray-200 dark:bg-gray-700': editor.isActive('bold') }"
                 class="p-1 rounded"
             >
-                bold
-            </button>
-            <button
+                <Bold />
+            </Button>
+            <Button
+                variant="outline"
                 @click="editor.chain().focus().toggleItalic().run()"
                 :disabled="!editor.can().chain().focus().toggleItalic().run()"
                 :class="{ 'bg-gray-200 dark:bg-gray-700': editor.isActive('italic') }"
                 class="p-1 rounded"
             >
-                italic
-            </button>
-            <button
+                <Italic />
+            </Button>
+            <Button
+                variant="outline"
                 @click="editor.chain().focus().toggleUnderline().run()"
                 :disabled="!editor.can().chain().focus().toggleUnderline().run()"
                 :class="{ 'bg-gray-200 dark:bg-gray-700': editor.isActive('underline') }"
                 class="p-1 rounded"
             >
-                underline
-            </button>
-            <button
+                <Underline />
+            </Button>
+            <Button
+                variant="outline"
                 @click="editor.chain().focus().toggleHeading({level: 1}).run()"
                 :disabled="!editor.can().chain().focus().toggleHeading({level: 1}).run()"
                 :class="{ 'bg-gray-200 dark:bg-gray-700': editor.isActive('heading', {level: 1}) }"
                 class="p-1 rounded"
             >
-                H1
-            </button>
-            <button
+                <Heading1 />
+            </Button>
+            <Button
+                variant="outline"
                 @click="editor.chain().focus().toggleHeading({level: 2}).run()"
                 :disabled="!editor.can().chain().focus().toggleHeading({level: 2}).run()"
                 :class="{ 'bg-gray-200 dark:bg-gray-700': editor.isActive('heading', {level: 2}) }"
                 class="p-1 rounded"
             >
-                H2
-            </button>
-            <button
+                <Heading2 />
+            </Button>
+            <Button
+                variant="outline"
                 @click="editor.chain().focus().toggleBulletList().run()"
                 :disabled="!editor.can().chain().focus().toggleBulletList().run()"
                 :class="{ 'bg-gray-200 dark:bg-gray-700': editor.isActive('bulletList') }"
                 class="p-1 rounded"
             >
-                List
-            </button>
-            <button
+                <List />
+            </Button>
+            <Button
+                variant="outline"
                 @click="editor.chain().focus().toggleOrderedList().run()"
                 :disabled="!editor.can().chain().focus().toggleOrderedList().run()"
                 :class="{ 'bg-gray-200 dark:bg-gray-700': editor.isActive('orderList') }"
                 class="p-1 rounded"
             >
-                OrderList
-            </button>
-            <button
+                <ListOrdered />
+            </Button>
+            <Button
+                variant="outline"
                 @click="editor.chain().focus().toggleCodeBlock().run()"
                 :disabled="!editor.can().chain().focus().toggleCodeBlock().run()"
                 :class="{ 'bg-gray-200 dark:bg-gray-700': editor.isActive('codeBlock') }"
                 class="p-1 rounded"
             >
-                CodeBlock
-            </button>
-            <button
+                <Code />
+            </Button>
+            <Button
+                variant="outline"
                 @click="editor.chain().focus().toggleBlockquote().run()"
                 :disabled="!editor.can().chain().focus().toggleBlockquote().run()"
                 :class="{ 'bg-gray-200 dark:bg-gray-700': editor.isActive('blockquote') }"
                 class="p-1 rounded"
             >
-                Blockquote
-            </button>
-            <button
+                <Quote />
+            </Button>
+            <Button
+                variant="outline"
                 @click="editor.chain().focus().setHorizontalRule().run()"
                 :disabled="!editor.can().chain().focus().setHorizontalRule().run()"
                 class="p-1"
             >
-                HR
-            </button>
-            <button
+                <SquareMinus />
+            </Button>
+            <Button
+                variant="outline"
                 @click="editor.chain().focus().undo().run()"
                 :disabled="!editor.can().chain().focus().undo().run()"
                 class="p-1 disabled:text-gray-400"
             >
-                Undo
-            </button>
-            <button
+                <Undo />
+            </Button>
+            <Button
+                variant="outline"
                 @click="editor.chain().focus().redo().run()"
                 :disabled="!editor.can().chain().focus().redo().run()"
                 class="p-1 disabled:text-gray-400"
             >
-                Redo
-            </button>
+                <Redo />
+            </Button>
         </section>
         <EditorContent :editor="editor" />
     </div>
@@ -122,5 +149,9 @@ const editor = useEditor({
 
 :deep(.tiptap pre) {
     @apply dark:bg-gray-100 dark:text-gray-900 bg-gray-700 text-gray-100
+}
+
+:deep(.tiptap li p) {
+    @apply m-0
 }
 </style>
